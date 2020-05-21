7a24f21a7aeaa1888c282fe9f7266027b90b4728
Simon <simonh72498@gmail.com>
2020-05-21 01:10:57 -0400

    finished mvp

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -44,15 +44,9 @@ function App() {
   const postNewUser = newUser => {
     axios.post("https://reqres.in/api/users", newUser)
       .then(res => {
-        setUsers([res.data, ...users])
+        setUsers([...users, res.data])
       })
       .finally(setUserValues(initialUserValues))
-  }
-
-  // get
-  const getUsers = () => {
-    axios.get("https://reqres.in/api/users")
-      .then(res => setUsers(res.data))
   }
   
   // for form onSubmit
@@ -73,8 +67,6 @@ function App() {
     setUserValues({...userValues, [name]: checked});
   }
 
-  useEffect(() => getUsers(), [])
-
   useEffect(() => {
     formSchema.isValid(userValues)
       .then(valid => {
@@ -86,6 +78,16 @@ function App() {
     <div className="App">
       <header><h1>User App</h1></header>
       <Form values={userValues} onInputChange={onInputChange} onSubmit={onSubmit} disabled={disabled} errors={formErrors} onCheckboxChange={onCheckboxChange}/>
+
+    {
+      users.map(user => (
+        <div>
+          <h1>{user.username}</h1>
+          <p>{user.userPassword}</p>
+          <p>{user.email}</p>
+        </div>
+      ))
+    }
 
     </div>
   );
