b9f493e05151b9c2bf1af2f4b13f60fba4a0abdc
Simon <simonh72498@gmail.com>
2020-05-21 01:31:16 -0400

    added style stretch goal

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -4,6 +4,17 @@ import axios from "axios";
 import * as yup from "yup";
 import formSchema from "./validation/formSchema";
 import './App.css';
+import styled from "styled-components";
+
+const StyledH1 = styled.h1`
+  font-weight: bold;
+  font-size: 32px;
+  margin: 10px 0 10px 0;
+`;
+
+const StyledH2 = styled.h2`
+  font-size: 24px;
+`;
 
 function App() {
   const initialUserList = [];
@@ -76,15 +87,17 @@ function App() {
 
   return (
     <div className="App">
-      <header><h1>User App</h1></header>
+      <header><StyledH1>User App</StyledH1></header>
       <Form values={userValues} onInputChange={onInputChange} onSubmit={onSubmit} disabled={disabled} errors={formErrors} onCheckboxChange={onCheckboxChange}/>
-
+      <br></br>
+      <StyledH1>Users</StyledH1>
+      <br></br>
     {
       users.map(user => (
         <div>
-          <h1>{user.username}</h1>
-          <p>{user.userPassword}</p>
-          <p>{user.email}</p>
+          <StyledH2>{user.username}</StyledH2>
+          <p>Password: {user.userPassword}</p>
+          <p>Email: {user.email}</p>
         </div>
       ))
     }
